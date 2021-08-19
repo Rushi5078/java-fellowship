5818f24f6a05082a9bed6d2c932f7a03f0c1bffc
Rushi5078 <88040056+Rushi5078@users.noreply.github.com>
2021-08-20 00:38:54 +0530

    Added:initial commit

---
+++ UC2.java
@@ -0,0 +1,11 @@
+package Snakeladder;
+
+public class UC2 {
+	
+	public static void main(String[ ] args) {
+		System.out.println("Snake And ladder Game using Single Player");
+		double Rolling = Math.floor(Math.random() * 10 ) % 6 + 1;
+	}
+	}
+
+
